65ccfe5b78fd423ff4e1996473ef9d324f9e5ae8
3TUSK <urey.s.knowledge@gmail.com>
2019-02-08 12:45:41 -0800

    Proof of concept: a mod with 99% of jsons and 1% of java code

---
+++ src/main/java/info/tritusk/beginnerbundle/BeginnerBundle.java
@@ -0,0 +1,6 @@
+package info.tritusk.beginnerbundle;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "beginner_bundle", name = "Beginner Bundle", version = "@VERSION@", useMetadata = true)
+public final class BeginnerBundle {}
